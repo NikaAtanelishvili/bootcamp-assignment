9aa43d268a5d157ddd0ec187327e9b116a5f6fc6
NikaAtanelishvili <atanelishvilinika@gmail.com>
2023-02-05 18:39:31 +0400

    style: update form layout

--- src/components/layouts/FormLayout.tsx
+++ src/components/layouts/FormLayout.tsx
@@ -14,37 +14,45 @@ const FormLayout: React.FC<FormLayoutProps> = props => {
   const navigate = useNavigate()
 
   return (
-    <div>
-      <header>
-        <Link
-          className=" h-10 w-10 flex justify-center items-center rounded-[50%] bg-[#FFFFFF]"
-          to="/"
-        >
-          <NavigationArrow />
-        </Link>
-        <div className="flex flex-row justify-between border-b border-[#1A1A1A]">
-          <h1 className=" font-bold text-[#1A1A1A] text-2xl leading-7">
-            {props.formTitle}
-          </h1>
-          <p>{props.page}/3</p>
-        </div>
-      </header>
-      <div>{props.children}</div>
-      <div className="flex flex-row justify-between">
-        {props.back && (
-          <button
-            onClick={() => navigate(props.back!)}
-            className="fles justify-center items-center bg-[#6B40E3] rounded w-[151px] h-12 font-medium text-[#FFFFFF] text-base leading-5 tracking-[0.08em]"
+    <div className=" h-screen max-w-[1098px] px-32 bg-[#F9F9F9]">
+      <div className="relative h-full">
+        <header className=" mb-[69px]">
+          <Link
+            className=" h-10 w-10 flex justify-center items-center rounded-[50%] bg-[#FFFFFF]"
+            to="/"
           >
-            უკან
-          </button>
-        )}
-        <button
-          onClick={() => navigate(props.to)}
-          className="fles justify-center items-center bg-[#6B40E3] rounded w-[151px] h-12 font-medium text-[#FFFFFF] text-base leading-5 tracking-[0.08em]"
+            <NavigationArrow />
+          </Link>
+          <div className=" pb-3 flex flex-row justify-between border-b border-[#1A1A1A]">
+            <h1 className=" font-Helvetica font-bold text-[#1A1A1A] text-2xl leading-7">
+              {props.formTitle}
+            </h1>
+            <p className=" font-Helvetica font-normal text-[#1A1A1A] text-xl leading-6">
+              {props.page}/3
+            </p>
+          </div>
+        </header>
+        <div>{props.children}</div>
+        <div
+          className={`absolute w-full bottom-16 flex flex-row ${
+            props.back ? 'justify-between' : 'justify-end'
+          } `}
         >
-          შემდეგი
-        </button>
+          {props.back && (
+            <button
+              onClick={() => navigate(props.back!)}
+              className="font-Helvetica bg-[#6B40E3] rounded w-[151px] h-12 font-medium text-[#FFFFFF] text-base leading-5 tracking-[0.08em]"
+            >
+              უკან
+            </button>
+          )}
+          <button
+            onClick={() => navigate(props.to)}
+            className=" font-Helvetica bg-[#6B40E3] rounded w-[151px] h-12 font-medium text-[#FFFFFF] text-base leading-5 tracking-[0.08em]"
+          >
+            შემდეგი
+          </button>
+        </div>
       </div>
     </div>
   )
