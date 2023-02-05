196e7931ec466a281625d06944543a4de7a439b4
NikaAtanelishvili <atanelishvilinika@gmail.com>
2023-02-05 18:58:52 +0400

    style: add svgs

---
+++ src/components/svgs/index.ts
@@ -0,0 +1,5 @@
+export { default as RedberryLogo } from './RedberryLogo'
+export { default as NavigationArrow } from './NavigationArrow'
+export { default as CheckIcon } from './CheckIcon'
+export { default as ErrorIcon } from './ErrorIcon'
+export { default as CalendarIcon } from './CalendarIcon'
