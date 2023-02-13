5874f3b7463e1469efe34530b80cf79e45aa1700
NikaAtanelishvili <atanelishvilinika@gmail.com>
2023-02-13 22:44:57 +0400

    chore: absolute imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Input } from './Input'
-export { default as Textarea } from './Textarea'
-export {default as HomeButton} from './HomeButton'
+export { default as HomeButton } from './HomeButton'
 
 export * from './svgs'
 export * from './layouts'
--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,1 +1,2 @@
 export { default as FormLayout } from './FormLayout'
+export { default as ResumeLayout } from './ResumeLayout'
